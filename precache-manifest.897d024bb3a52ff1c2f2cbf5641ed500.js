8225c2e86d50476360d7242986ab9004a2210814
muhammad hasnain <hasnainsaleem2233@gmail.com>
2019-08-26 14:37:40 +0500

    first commit

---
+++ precache-manifest.897d024bb3a52ff1c2f2cbf5641ed500.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2b1166e9fc653f346a25a9979fa9e31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "968d0c2a1be9253d8a84",
+    "url": "/static/css/2.7d96b6c7.chunk.css"
+  },
+  {
+    "revision": "462fdc805351144163a1",
+    "url": "/static/css/main.f9e23133.chunk.css"
+  },
+  {
+    "revision": "968d0c2a1be9253d8a84",
+    "url": "/static/js/2.2f892cf2.chunk.js"
+  },
+  {
+    "revision": "462fdc805351144163a1",
+    "url": "/static/js/main.14cd20db.chunk.js"
+  },
+  {
+    "revision": "5b06b92bf662c1e82036",
+    "url": "/static/js/runtime~main.5cda6740.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "5aa5ab4f0f595699d677ac22d1a146f9",
+    "url": "/static/media/hasnain2.5aa5ab4f.jpg"
+  }
+]);
